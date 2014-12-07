e83b66aa8cd9b7f7cddeee219b0ef2ae7235dc8f
kevlu94 <kevlu94@gmail.com>
2014-12-06 16:49:32 -0800

    smoothing for movement in changing elevation

--- heightMap.js
+++ heightMap.js
@@ -15,10 +15,10 @@ for(var x=1; x<size; x++) {
     for(var z=1; z<size; z++) {
         var rand=Math.random();
         if(rand>=0.3) {
-            heights[x][z]=((heights[x-1][z-1]+heights[x][z-1]+heights[x-1][z])/3)+(0.3*Math.random());
+            heights[x][z]=((heights[x-1][z-1]+heights[x][z-1]+heights[x-1][z])/3)+(0.5*Math.random());
         }
         else {
-            heights[x][z]=((heights[x-1][z-1]+heights[x][z-1]+heights[x-1][z])/3)-(0.3*Math.random());
+            heights[x][z]=((heights[x-1][z-1]+heights[x][z-1]+heights[x-1][z])/3)-(0.5*Math.random());
         }
     }
 }
--- map.js
+++ map.js
@@ -1,10 +1,25 @@
 var heightOf = function(xPos, zPos)
 {
 	// EDIT when we add varying elevation
-    if(heights[Math.trunc(xPos)]!=null && heights[Math.trunc(xPos)][Math.trunc(zPos)]!=null) {
-        return heights[Math.trunc(xPos)][Math.trunc(zPos)];
+    var mixX = xPos - Math.trunc(xPos);
+	var mixZ = zPos - Math.trunc(zPos);
+	var a = vec2(Math.trunc(xPos), Math.trunc(zPos));
+	var b = vec2(Math.trunc(xPos) + 1, Math.trunc(zPos));
+	var c = vec2(Math.trunc(xPos), Math.trunc(zPos) + 1);
+	var d = vec2(Math.trunc(xPos) + 1, Math.trunc(zPos) + 1);
+
+    if(		heights[Math.trunc(xPos)]!=null
+    	&& 	heights[Math.trunc(xPos)][Math.trunc(zPos)]!=null
+    	&&	heights[Math.trunc(xPos) + 1]!=null
+    	&&	heights[Math.trunc(xPos) + 1][Math.trunc(zPos) + 1]!=null)
+    {
+        return 		heights[a[0]][a[1]] * (1.0 - mixX) * (1.0 - mixZ)
+				+	heights[b[0]][b[1]] * mixX * (1.0 - mixZ)
+				+	heights[c[0]][c[1]] * (1.0 - mixX) * mixZ
+				+	heights[d[0]][d[1]] * mixX * mixZ;
     }
     else {
         return 0.0;
     }
 }
+
