d3879afb94115e5994cffcce802576a1c3c2fac4
Kyle Kodani <poolioman@sbcglobal.net>
2014-12-07 18:31:15 -0800

    added textures to terrain added rolling hills, bay, forest fixed z-fighting

--- island/island.js
+++ island/island.js
@@ -5,6 +5,19 @@ var Island = (function() {
     var nbo = null;
     var tbo = null;
 	var tanbo = null;
+    
+    var mtnvbo = null;
+    var mtnnbo = null;
+    var mtntbo = null;
+    
+    var mtn2vbo = null;
+    var mtn2nbo = null;
+    var mtn2tbo = null;
+    
+    var sandvbo = null;
+    var sandnbo = null;
+    var sandtbo = null;
+    
 
     var groundTexture;
     var groundMaterial = new Material(
@@ -12,10 +25,26 @@ var Island = (function() {
         vec4(0.8, 0.7, 0.7, 1.0)
     );
     
+    var mtnTexture;
+    var mtn2Texture;
+    var sandTexture;
+    
     var vertices = [];
     var normals = [];
     var texCoordinates = [];
 	var tangents = [];
+    
+    var mtnVertices = [];
+    var mtnNormals = [];
+    var mtnTexCoordinates = [];
+    
+    var mtn2Vertices = [];
+    var mtn2Normals = [];
+    var mtn2TexCoordinates = [];
+    
+    var sandVertices = [];
+    var sandNormals = [];
+    var sandTexCoordinates = [];
 
     var plane = function(a, b, c) {
         var ab = subtract(b,a);
@@ -31,22 +60,111 @@ var Island = (function() {
             var lr = [x+1, heights[x+1][z], z];
             var ur = [x+1, heights[x+1][z+1], z+1];
         
-            vertices.push(ll);
-            vertices.push(ul);
-            vertices.push(ur);
-            vertices.push(ur);
-            vertices.push(lr);
-            vertices.push(ll);
+            if(ul[1]>=7 && ul[1]<15) {
             
-            var n1=plane(ul,ur,ll);
-            var n2=plane(lr,ll,ur);
+                mtnVertices.push(ll);
+                mtnVertices.push(ul);
+                mtnVertices.push(ur);
+                mtnVertices.push(ur);
+                mtnVertices.push(lr);
+                mtnVertices.push(ll);
+              
+                var n1=plane(ul,ur,ll);
+                var n2=plane(lr,ll,ur);
             
-            normals.push(n1);
-            normals.push(n1);
-            normals.push(n1);
-            normals.push(n2);
-            normals.push(n2);
-            normals.push(n2);
+                mtnNormals.push(n1);
+                mtnNormals.push(n1);
+                mtnNormals.push(n1);
+                mtnNormals.push(n2);
+                mtnNormals.push(n2);
+                mtnNormals.push(n2);
+            
+                mtnTexCoordinates.push(0.0, 1.0);
+                mtnTexCoordinates.push(1.0, 1.0);
+                mtnTexCoordinates.push(1.0, 0.0);
+                mtnTexCoordinates.push(1.0, 0.0);
+                mtnTexCoordinates.push(0.0, 0.0);
+                mtnTexCoordinates.push(0.0, 1.0);
+            }
+            
+            else if(ul[1]>=15) {
+                mtn2Vertices.push(ll);
+                mtn2Vertices.push(ul);
+                mtn2Vertices.push(ur);
+                mtn2Vertices.push(ur);
+                mtn2Vertices.push(lr);
+                mtn2Vertices.push(ll);
+              
+                var n1=plane(ul,ur,ll);
+                var n2=plane(lr,ll,ur);
+            
+                mtn2Normals.push(n1);
+                mtn2Normals.push(n1);
+                mtn2Normals.push(n1);
+                mtn2Normals.push(n2);
+                mtn2Normals.push(n2);
+                mtn2Normals.push(n2);
+            
+                mtn2TexCoordinates.push(0.0, 1.0);
+                mtn2TexCoordinates.push(1.0, 1.0);
+                mtn2TexCoordinates.push(1.0, 0.0);
+                mtn2TexCoordinates.push(1.0, 0.0);
+                mtn2TexCoordinates.push(0.0, 0.0);
+                mtn2TexCoordinates.push(0.0, 1.0);
+            }
+            
+            else if(ul[1]<=0.2) {
+                sandVertices.push(ll);
+                sandVertices.push(ul);
+                sandVertices.push(ur);
+                sandVertices.push(ur);
+                sandVertices.push(lr);
+                sandVertices.push(ll);
+              
+                var n1=plane(ul,ur,ll);
+                var n2=plane(lr,ll,ur);
+            
+                sandNormals.push(n1);
+                sandNormals.push(n1);
+                sandNormals.push(n1);
+                sandNormals.push(n2);
+                sandNormals.push(n2);
+                sandNormals.push(n2);
+            
+                sandTexCoordinates.push(0.0, 1.0);
+                sandTexCoordinates.push(1.0, 1.0);
+                sandTexCoordinates.push(1.0, 0.0);
+                sandTexCoordinates.push(1.0, 0.0);
+                sandTexCoordinates.push(0.0, 0.0);
+                sandTexCoordinates.push(0.0, 1.0);
+            }
+            
+            else {
+            
+                vertices.push(ll);
+                vertices.push(ul);
+                vertices.push(ur);
+                vertices.push(ur);
+                vertices.push(lr);
+                vertices.push(ll);
+            
+                var n1=plane(ul,ur,ll);
+                var n2=plane(lr,ll,ur);
+            
+                normals.push(n1);
+                normals.push(n1);
+                normals.push(n1);
+                normals.push(n2);
+                normals.push(n2);
+                normals.push(n2);
+            
+                texCoordinates.push(0.0, 1.0);
+                texCoordinates.push(1.0, 1.0);
+                texCoordinates.push(1.0, 0.0);
+                texCoordinates.push(1.0, 0.0);
+                texCoordinates.push(0.0, 0.0);
+                texCoordinates.push(0.0, 1.0);
+            }
             
             texCoordinates.push(0.0, 1.0);
             texCoordinates.push(1.0, 1.0);
@@ -70,7 +188,10 @@ var Island = (function() {
 			throw "Unable to init Island data, gl not defined";
 		}
 
-        groundTexture = new Texture.fromImageSrc('images/sand.jpg')
+        groundTexture = new Texture.fromImageSrc('images/grass2.jpg');
+        mtnTexture = new Texture.fromImageSrc('images/rock.jpg');
+        mtn2Texture = new Texture.fromImageSrc('images/rock2.jpg');
+        sandTexture = new Texture.fromImageSrc('images/sand.jpg');
 
 		vbo = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
@@ -87,16 +208,58 @@ var Island = (function() {
 		tanbo = gl.createBuffer();
 		gl.bindBuffer(gl.ARRAY_BUFFER, tanbo);
 		gl.bufferData(gl.ARRAY_BUFFER, flatten(tangents), gl.STATIC_DRAW);
+
+        
+        mtnvbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtnvbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtnVertices), gl.STATIC_DRAW);
+
+		mtnnbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtnnbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtnNormals), gl.STATIC_DRAW);
+
+		mtntbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtntbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtnTexCoordinates), gl.STATIC_DRAW);
+        
+        mtn2vbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtn2vbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtn2Vertices), gl.STATIC_DRAW);
+
+		mtn2nbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtn2nbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtn2Normals), gl.STATIC_DRAW);
+
+		mtn2tbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, mtn2tbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(mtn2TexCoordinates), gl.STATIC_DRAW);
+        
+        sandvbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, sandvbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(sandVertices), gl.STATIC_DRAW);
+
+		sandnbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, sandnbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(sandNormals), gl.STATIC_DRAW);
+
+		sandtbo = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, sandtbo);
+		gl.bufferData(gl.ARRAY_BUFFER, flatten(sandTexCoordinates), gl.STATIC_DRAW);
+
 	};
     
     //first four args are corners of square
     var islandConstructor = function() {
     
-        if(!vbo || !nbo || !tbo) {
+        if(!vbo || !nbo || !tbo || !mtnvbo || !mtnnbo || !mtntbo || !mtn2vbo || !mtn2nbo || !mtn2tbo) {
 			initVertexData();
 		}
-
-		Shape.call(this, vbo, nbo, tanbo, tbo, null, vertices.length, groundMaterial, groundTexture);
+        
+        this.dunes=new Shape(vbo, nbo, tbo, null, vertices.length, groundMaterial, groundTexture);
+        this.mtn=new Shape(mtnvbo, mtnnbo, mtntbo, null, mtnVertices.length, null, mtnTexture);
+        this.mtn2=new Shape(mtn2vbo, mtn2nbo, mtn2tbo, null, mtn2Vertices.length, null, mtn2Texture);
+        this.sand=new Shape(sandvbo, sandnbo, sandtbo, null, sandVertices.length, null, sandTexture);
+        
     };
     
     return islandConstructor;
@@ -104,4 +267,11 @@ var Island = (function() {
 })();
 
 inheritPrototype(Island, Shape);
+
+Island.prototype.draw = function(dt, mat) {
+    this.dunes.draw(dt, mat);
+    this.mtn.draw(dt, mat);
+    this.mtn2.draw(dt, mat);
+    this.sand.draw(dt, mat);
+}
 
